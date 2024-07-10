db4e80ce0226d2054941ec98b9c5ea862b3ae7d8
dd-jiyun <dlwldbs8015@gmail.com>
2024-07-10 10:21:44 +0900

    refactor: @ResponseBody 어노테이션 추가

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import roomescape.dao.ReservationDAO;
 import roomescape.dto.RequestReservation;
 import roomescape.model.Reservation;
@@ -28,11 +29,13 @@ public class ReservationController {
     }
 
     @GetMapping("/reservations/{id}")
+    @ResponseBody
     public ResponseEntity<Reservation> reserveOne(@PathVariable Long id) {
         return ResponseEntity.ok().body(reservationDAO.findReservationById(id));
     }
 
     @GetMapping("/reservations")
+    @ResponseBody
     public ResponseEntity<List<Reservation>> reserveList() {
         return ResponseEntity.ok().body(reservationDAO.findAllReservations());
     }
